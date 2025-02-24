2cba59a75e227f61059587e9ba88922235d11717
James Farnsworth <jamescfar@gmail.com>
2025-02-24 15:12:15 -0500

    Publisher

---
+++ camera_publisher.py
@@ -0,0 +1,24 @@
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2 as cv
+import numpy as np
+import imutils
+
+rospy.init_node("camera_publisher", anonymous=True)
+
+publisher = rospy.Publisher("video topic", Image, queue_size=60)
+
+rate = rospy.Rate(30)
+
+videoCaptureObject = cv.VideoCapture(0)
+
+bridgeObject = CvBridge()
+
+while not rospy.is_shutdown() :
+    returnValue, capturedFrame = videoCaptureObject.read()
+    if returnValue == True :
+        rospy.loginfo("Video frame captured and published")
+        imageToTransmit = bridgeObject.cv2_to_imgmsg(capturedFrame)
+        publisher.publish(imageToTransmit)
+    rate.sleep()
