d29e332d63d25515897a0d5152889018b4475df3
James Farnsworth <jamescfar@gmail.com>
2025-02-26 23:17:17 -0500

    small optimization, only do inverse and determinant once

--- robot_math.py
+++ robot_math.py
@@ -6,6 +6,8 @@ class Gauss2D:
     def __init__(self, u_x, u_y, S) :
         self.u = np.matrix([u_x, u_y])
         self.S = S
+        self.invS = np.linalg.inv(self.S)
+        self.detS = np.linalg.det(self.S)
     
     def init_polar(self, u_d, u_theta, S) :
         self.u = np.matrix([u_d * math.cos(u_theta), u_d * math.sin(u_theta)])
@@ -13,10 +15,8 @@ class Gauss2D:
         self.S = R * S * R.T
 
     def probability(self, x, y) :
-        invS = np.linalg.inv(self.S)
-        detS = np.linalg.det(self.S)
         ud = np.matrix([x, y]) - self.u
-        return math.exp(-ud * invS * ud.T / 2) / math.sqrt(math.pow(2*math.pi, 2) * detS)
+        return math.exp(-ud * self.invS * ud.T / 2) / math.sqrt(math.pow(2*math.pi, 2) * self.detS)
 
 def gauss2D_from_polar(u_d, u_theta, S) :
     R = np.matrix([[math.cos(u_theta), -math.sin(u_theta)],[math.sin(u_theta), math.cos(u_theta)]])
