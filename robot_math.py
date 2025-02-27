ac25a3650084e56b3fa748d802cde7fc879ce03e
James Farnsworth <jamescfar@gmail.com>
2025-02-27 18:08:40 -0500

    rotated mean and input matrices to better match mathematical formulation

--- robot_math.py
+++ robot_math.py
@@ -4,21 +4,18 @@ import numpy as np
 class Gauss2D:
 
     def __init__(self, u_x, u_y, S) :
-        self.u = np.matrix([u_x, u_y])
+        self.u = np.matrix([[u_x], [u_y]])
         self.S = S
         self.invS = np.linalg.inv(self.S)
         self.detS = np.linalg.det(self.S)
-    
-    def init_polar(self, u_d, u_theta, S) :
-        self.u = np.matrix([u_d * math.cos(u_theta), u_d * math.sin(u_theta)])
-        R = np.matrix([[math.cos(u_theta), -math.sin(u_theta)],[math.sin(u_theta), math.cos(u_theta)]])
-        self.S = R * S * R.T
 
     def probability(self, x, y) :
-        ud = np.matrix([x, y]) - self.u
-        return math.exp(-ud * self.invS * ud.T / 2) / math.sqrt(math.pow(2*math.pi, 2) * self.detS)
+        ud = np.matrix([[x], [y]]) - self.u
+        return math.exp(-ud.T * self.invS * ud / 2) / math.sqrt(math.pow(2*math.pi, 2) * self.detS)
 
 def gauss2D_from_polar(u_d, u_theta, S) :
     R = np.matrix([[math.cos(u_theta), -math.sin(u_theta)],[math.sin(u_theta), math.cos(u_theta)]])
     S_v = R * S * R.T
     return Gauss2D(u_d * math.cos(u_theta), u_d * math.sin(u_theta), S_v)
+
+pdf = Gauss2D(10, 0, np.matrix([[2, 0],[0, 0.5]]))
