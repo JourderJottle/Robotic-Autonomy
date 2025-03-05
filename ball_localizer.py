ac5c1dcf23fd5914cfd1fe0d19f3ceae5668a045
victor-uvg-1 <Matthew Krinn>
2025-03-05 18:35:45 -0500

    Synced fixes on local copy of files to git copy

--- ball_localizer.py
+++ ball_localizer.py
@@ -5,13 +5,77 @@ import rospy
 from std_msgs.msg import Float32MultiArray
 import numpy as np
 import matplotlib.pyplot as plt
-from robot_math import *
+from mpl_toolkits.mplot3d import Axes3D
+import math
+import cv2 as cv
+
+from sensor_msgs.msg import PointCloud2, PointField
+import sensor_msgs.point_cloud2 as pc2
+
+import open3d as o3d
+
+
+
+class Gauss2D:
+
+    def __init__(self, u: np.matrix, S: np.matrix) :
+        self.u = u
+        self.S = S
+        
+        # add epsilon so no det=0 error
+        epsilon = 1e-6
+        self.S += np.eye(self.S.shape[0]) * epsilon
+        
+        self.invS = np.linalg.inv(self.S)
+        self.detS = np.linalg.det(self.S)
+
+    def probability(self, x: np.matrix) -> float :
+        ud = x - self.u
+        
+        exponent = -0.5 * (ud.T @ self.invS @ ud).item()
+        
+        exponent = np.clip(exponent, -700, 700)
+        
+        return math.exp(exponent) / math.sqrt( (2*math.pi)**2 * self.detS)
+        
+        # return math.exp(-ud.T * self.invS * ud / 2) / math.sqrt(math.pow(2*math.pi, 2) * self.detS)
+
+def rotational_matrix(theta: float) -> np.matrix :
+    return np.array([[math.cos(theta), -math.sin(theta)],[math.sin(theta), math.cos(theta)]], dtype=np.float64)
+
+def gauss2D_from_polar(u_d: float, u_theta: float, S: np.matrix) -> Gauss2D :
+    R = rotational_matrix(u_theta)
+    S_v = R @ S @ R.T
+    return Gauss2D(np.array([[u_d * math.cos(u_theta)], [u_d * math.sin(u_theta)]], dtype=np.float64), S_v)
+
+def local_target_pose_to_global(target_pose: np.ndarray, robot_pose: np.ndarray, robot_theta: float) -> np.ndarray :
+    #global_target_pose = target_pose + robot_pose
+    #R = rotational_matrix(robot_theta)
+    #return R * global_target_pose
+    
+    # Must rotate before adding
+    R = rotational_matrix(robot_theta)
+    return R @ target_pose + robot_pose
+    
+    
+    
+
+
+
 
 class BallLocalizer :
     def __init__(self) :
         rospy.init_node("ball_localizer", anonymous=True)
         rospy.Subscriber("/ball_data", Float32MultiArray, self.callback)
+        #self.fig, self.ax = plt.subplots(subplot_kw={"projection":"3d"})
+        
+      
         self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111, projection='3d')
+        
+        
+        self.cloud_pub = rospy.Publisher('/ball_cloud', PointCloud2, queue_size=10)
+        
         rospy.spin()
 
     def variance_from_distance(self, distance) :
@@ -38,19 +102,45 @@ class BallLocalizer :
         
         # Generating the density function
         # for each point in the meshgrid
+        
+        points = []
         pdf = np.zeros(X.shape)
+        
         for i in range(X.shape[0]):
-            for j in range(X.shape[1]):
-                pdf[i,j] = dist.probability(np.matrix([[X[i,j]], [Y[i,j]]]))
+        	for j in range(X.shape[1]):
+        		prob = dist.probability(np.array([[X[i, j]], [Y[i, j]]], dtype=np.float64))
+        		p = [ X[i, j], Y[i, j], prob ]
+        		points.append(p)
+        
+       	header = rospy.Header()
+       	header.stamp = rospy.Time.now()
+       	header.frame_id = "map"
+       	
+        fields = [
+            PointField('x', 0, PointField.FLOAT32, 1),
+            PointField('y', 4, PointField.FLOAT32, 1),
+            PointField('z', 8, PointField.FLOAT32, 1),
+        ]
+        
+        cloud_msg = pc2.create_cloud(header, fields, points)
+        cloud_msg.width = len(points)
+        cloud_msg.height = 1
+        cloud_msg.is_dense = False
+        self.cloud_pub.publish(cloud_msg)
+        rospy.loginfo("Published ball cloud")
+        
         
         # Plotting the density function values
-        ax = self.fig.add_subplot(131, projection = '3d')
-        ax.plot_surface(X, Y, pdf, cmap = 'viridis')
-        plt.xlabel("x1")
-        plt.ylabel("x2")
-        plt.title(f'Distribution')
-        ax.axes.zaxis.set_ticks([])
-        plt.show()
+        #ax = self.fig.add_subplot(111, projection = '3d')
+        #ax.plot_surface(X, Y, pdf, cmap = 'viridis')
+        #plt.xlabel("x1")
+        #plt.ylabel("x2")
+        #plt.title(f'Distribution')
+        #ax.axes.zaxis.set_ticks([])
+        #plt.show()
+
+        
+
 
 if __name__ == '__main__':
     try:
