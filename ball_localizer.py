3625b10cb3edaa76f745f59264518d893affe5a7
James Farnsworth <jamescfar@gmail.com>
2025-03-24 14:37:03 -0400

    debug stuff

--- ball_localizer.py
+++ ball_localizer.py
@@ -60,9 +60,10 @@ def derive_gradient(func, location, dl) :
 
 def ekf_predict(previous_state, previous_covariance, input, motion_model, motion_noise, dt) :
     A = derive_gradient(motion_model, input, 0.1)
-    
+    a1 = A @ previous_covariance
+    a2 = a1 @ A.T    
     # Returns (mean, covariance)
-    return (previous_state + dt * motion_model(input), A @ previous_covariance @ A.T + motion_noise)
+    return (previous_state + dt * motion_model(input), a2 + motion_noise)
 
 
 def ekf_correct(predicted_state, predicted_covariance, observation, sensor_model, sensor_noise) :
