13fc2985df2b599c619364d0c3dfc26b9f7e96ec
MatthewKrinn <123118912+MatthewKrinn@users.noreply.github.com>
2025-03-24 17:19:00 -0400

    noway

--- camera_subscriber.py
+++ camera_subscriber.py
@@ -44,7 +44,7 @@ class BallTracker:
                 ### Hmmm, the problem is that we always have the x,y but sometimes the depth is zero at that point
                 ## Thus, save the previous value and if the ball is inframe but it says zero, use the old number
                 
-                x, y = self.ball_2d_data[0], self.ball_2d_data[1]
+                x, y, theta = self.ball_2d_data[0], self.ball_2d_data[1], self.ball_2d_data[2]
                 
                 # 
                 
@@ -57,7 +57,7 @@ class BallTracker:
                     depth_at_center = self.past_depth
                 
                 
-                rospy.loginfo(f'Found ball at {x, y}\nso depth is {depth_at_center:.2f}')
+                rospy.loginfo(f'Ball {x, y}, depth: {depth_at_center:.2f}, theta: {theta:.2f}')
                 
                 region = cv_img[max(0, y - 2):y + 3, max(0, x - 2):x + 3]  # 5x5 region
 
