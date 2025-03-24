18186881d3664884f18b5677708cc1a804d14300
MatthewKrinn <123118912+MatthewKrinn@users.noreply.github.com>
2025-03-24 14:02:06 -0400

    Comments and Pseudo inverse instead of normal inv

--- EKF.py
+++ EKF.py
@@ -15,11 +15,16 @@ def derive_gradient(func, location, dl) :
 
 def predict(previous_state, previous_covariance, input, motion_model, motion_noise, dt) :
     A = derive_gradient(motion_model, input, 0.1)
+    
+    # Returns (mean, covariance)
     return (previous_state + dt * motion_model(input), A @ previous_covariance @ A.T + motion_noise)
+
 
 def correct(predicted_state, predicted_covariance, observation, sensor_model, sensor_noise) :
     C = derive_gradient(sensor_model, predicted_state, 0.1)
-    K = predicted_covariance @ C.T @ np.linalg.inv(C @ predicted_covariance @ C.T + sensor_noise)
+    K = predicted_covariance @ C.T @ np.linalg.pinv(C @ predicted_covariance @ C.T + sensor_noise)
+    
+    # Returns (mean, covariance)
     return (predicted_state + K @ (observation - sensor_model(predicted_state)), (np.eye(len(predicted_state)) - K @ C) @ predicted_covariance)
 
 
