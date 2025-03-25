6ea9bdae7f319c856718baf411b1ae9f228b29b9
MatthewKrinn <123118912+MatthewKrinn@users.noreply.github.com>
2025-03-25 19:22:38 -0400

    comments

--- ball_localizer.py
+++ ball_localizer.py
@@ -147,6 +147,7 @@ class BallLocalizer :
         dt = time - self.last_time
         self.last_time = time
 
+        # Check more precisely that distance != 0
         if data.data != None and len(data.data) > 0 and data.data[0] != 0:
             init_distance = data.data[0]
             init_theta = data.data[1]
