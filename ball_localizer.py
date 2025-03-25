cc2833646f8a4118bbe24d0663eca34378e9c588
MatthewKrinn <123118912+MatthewKrinn@users.noreply.github.com>
2025-03-25 19:58:25 -0400

    clear queue if you lose the ball

--- ball_localizer.py
+++ ball_localizer.py
@@ -149,11 +149,12 @@ class BallLocalizer :
         dt = time - self.last_time
         self.last_time = time
         
-        if data.data[0] == 0:
-            self.observation_queue.clear()
-            self.distance_total = 0
-            self.angle_total = 0
-            
+        
+        if data.data != None:
+            if data.data[0] == 0:
+                self.observation_queue.clear()
+                self.distance_total = 0
+                self.angle_total = 0
 
         # Check more precisely that distance != 0
         if data.data != None and len(data.data) > 0 and data.data[0] != 0:
