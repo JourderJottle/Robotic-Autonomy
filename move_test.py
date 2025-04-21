15abf203e104e324da677fc907d24a0d5899b3b3
MatthewKrinn <123118912+MatthewKrinn@users.noreply.github.com>
2025-04-21 16:00:12 -0400

    ok

--- move_test.py
+++ move_test.py
@@ -4,5 +4,12 @@ from movement import Controller
 if __name__ == '__main__':
     rospy.init_node("Navegator", anonymous=True) 
     controller = Controller()
+    rospy.sleep(0.5)
+    
     controller.send_movement(0.3, 0)
+    rospy.sleep(0.25)
+    
+    
     controller.send_movement(0.0, 0.0)
+    rospy.sleep(0.25)
+
