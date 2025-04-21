0e66c36fd62a324c47c19b40195ff453746184d3
MatthewKrinn <123118912+MatthewKrinn@users.noreply.github.com>
2025-04-21 16:05:24 -0400

    ok

--- move_test.py
+++ move_test.py
@@ -2,19 +2,18 @@
 
 
 import rospy
-from ball_localizer import BallLocalizer
 
-# from movement import Controller
+from movement import Controller
 
-# if __name__ == '__main__':
-#     rospy.init_node("Navegator", anonymous=True) 
-#     controller = Controller()
-#     rospy.sleep(0.5)
+if __name__ == '__main__':
+    rospy.init_node("Navegator", anonymous=True) 
+    controller = Controller()
+    rospy.sleep(0.5)
     
-#     controller.send_movement(0.3, 0)
-#     rospy.sleep(0.25)
+    controller.send_movement(0.3, 0)
+    rospy.sleep(0.25)
     
     
-#     controller.send_movement(0.0, 0.0)
-#     rospy.sleep(0.2)
+    controller.send_movement(0.0, 0.0)
+    rospy.sleep(0.2)
 
