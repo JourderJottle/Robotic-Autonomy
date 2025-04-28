9ec2f5441e9168a6fad9ebb9b254addeb7752f37
Matthew Krinn <matthewkrinn@gmail.com>
2025-04-28 16:07:01 -0400

    playing with colors

--- ball_localizer.py
+++ ball_localizer.py
@@ -75,7 +75,7 @@ def derive_gradient(func, location, dl) :
 def ekf_predict(previous_state, previous_covariance, input, motion_model, motion_noise, dt) :
     A = derive_gradient(motion_model, input, 0.1)
     # Returns (mean, covariance)
-    return (previous_state + dt * motion_model, A @ previous_covariance @ A.T + motion_noise)
+    return (previous_state + dt * motion_model(input), A @ previous_covariance @ A.T + motion_noise)
 
 def ekf_correct(predicted_state, predicted_covariance, observation, sensor_model, sensor_noise) :
     C = derive_gradient(sensor_model, predicted_state, 0.1)
